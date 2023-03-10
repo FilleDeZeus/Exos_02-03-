dddbf524fbf41ce5b40d8c117afc60b92b3bee3d
FilleDeZeus <enide26@hotmail.fr>
2023-03-10 12:43:55 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { useRoutes } from 'react-router-dom';
+import {Donnee} from './components/donnee/donnee.js'
+//composant App
 export const App = () => {
+  //les etats de ce composant
+  //l'ensemble des routes 
+  let routes = useRoutes ([
+    {
+      path : "/",
+      element : <Donnee />
+    }
+    
+  ])
+    
   return (
-    <div>App</div>
-  )
-}
+    <div className="container">
+      {/* compo navbar et ses props */}
+      {/* affichage des routes */}
+      {routes}
+</div>
+  );
+};
